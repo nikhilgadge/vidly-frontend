d69ed3f3ad77f39b659270a062d19e93ccb042e0
1 <a1@1s-MacBook-Pro.local>
2024-05-18 16:04:17 +0530

    hogvug

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/api";
+const baseUrl = process.env.REACT_APP_API_BASE_URL || "http://localhost:3001/api";
 console.log(baseUrl);
 
 const api = {
